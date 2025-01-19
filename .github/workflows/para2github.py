28c367db75b29ca2178c2295a40e70309a1b449c
Wulian <1055917385@qq.com>
2025-01-19 12:33:08 +0800

    Update para2github.py

--- .github/workflows/para2github.py
+++ .github/workflows/para2github.py
@@ -2,7 +2,6 @@ import json
 import os
 import re
 import sys
-from collections import OrderedDict
 from pathlib import Path
 from typing import Tuple
 
@@ -11,6 +10,10 @@ import requests
 TOKEN = os.getenv("API_TOKEN", "")
 PROJECT_ID = os.getenv("PROJECT_ID", "")
 FILE_URL = f"https://paratranz.cn/api/projects/{PROJECT_ID}/files/"
+
+file_id_list = []
+file_path_list = []
+zh_cn_list = []
 
 if len(TOKEN) != 32 or not PROJECT_ID.isdigit():
     raise EnvironmentError("未设置有效的 API_TOKEN 或 PROJECT_ID 环境变量")
@@ -40,11 +43,15 @@ def translate(file_id: int) -> Tuple[list[str], list[str]]:
     return keys, values
 
 
-def get_files() -> list[Tuple[int, str]]:
-    """获取项目中的文件列表并提取文件ID和路径"""
+def get_files() -> None:
+    """
+    获取项目中的文件列表并提取文件ID和路径
+    """
     headers = {"Authorization": TOKEN, "accept": "*/*"}
     files = fetch_json(FILE_URL, headers)
-    return [(file["id"], file["name"]) for file in files]
+    for file in files:
+        file_id_list.append(file["id"])
+        file_path_list.append(file["name"])
 
 
 def save_translation(zh_cn_dict: dict[str, str], path: Path) -> None:
@@ -57,7 +64,9 @@ def save_translation(zh_cn_dict: dict[str, str], path: Path) -> None:
     dir_path = Path("CNPack") / path.parent
     Path(str(dir_path).replace("rename/", "")).mkdir(parents=True, exist_ok=True)
     file_path = dir_path / "zh_cn.json"
-    source_path = str(file_path).replace("zh_cn.json", "en_us.json").replace("CNPack", "Source")
+    source_path = (
+        str(file_path).replace("zh_cn.json", "en_us.json").replace("CNPack", "Source")
+    )
     if "rename" in str(file_path):
         file_path = str(file_path).replace("rename/", "")
     with open(file_path, "w", encoding="UTF-8") as f:
@@ -67,11 +76,19 @@ def save_translation(zh_cn_dict: dict[str, str], path: Path) -> None:
             keys = source_json.keys()
             for key in keys:
                 source_json[key] = zh_cn_dict[key]
-            json.dump(source_json, f, ensure_ascii=False, indent=4, separators=(",", ":"))
+            json.dump(
+                source_json, f, ensure_ascii=False, indent=4, separators=(",", ":")
+            )
         except IOError:
             print(f"{source_path}路径不存在，文件按首字母排序！")
-            json.dump(zh_cn_dict, f, ensure_ascii=False, indent=4, separators=(",", ":"),sort_keys=True)
-
+            json.dump(
+                zh_cn_dict,
+                f,
+                ensure_ascii=False,
+                indent=4,
+                separators=(",", ":"),
+                sort_keys=True,
+            )
 
 
 def process_translation(file_id: int, path: Path) -> dict[str, str]:
@@ -82,27 +99,30 @@ def process_translation(file_id: int, path: Path) -> dict[str, str]:
     :return: 翻译内容字典
     """
     keys, values = translate(file_id)
+    # 替换换行符
     zh_cn_dict = {key: re.sub(r"\\n", "\n", value) for key, value in zip(keys, values)}
-
+    # 特殊处理：ftbquest 文件
     if "ftbquest" in path.name:
         zh_cn_dict = {
             key: value.replace(" ", "\u00A0") if "image" not in value else value
             for key, value in zip(keys, values)
         }
-
     return zh_cn_dict
 
 
 def main() -> None:
     if sys.version_info < (3, 9):
         raise EnvironmentError("请使用 Python 3.9 及更高版本")
-
-    for file_id, path in get_files():
-        if "TM" in path:  # 机械动力：星辰专用
+    get_files()
+    for file_id, path in zip(file_id_list, file_path_list):
+        if "TM" in path:  # 跳过 TM 文件
             continue
-
         zh_cn_dict = process_translation(file_id, Path(path))
+        zh_cn_list.append(zh_cn_dict)
+
         save_translation(zh_cn_dict, Path(path))
+        if "rename" in path:
+            path = str(path).replace("rename/", "")
         print(f"已从Patatranz下载到仓库：{re.sub('en_us.json', 'zh_cn.json', path)}")
 
 
