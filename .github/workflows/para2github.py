ea18ba4cb69c9648537fc41a8a41e2129427d434
nageih <zhwlang2008@outlook.com>
2025-02-07 12:53:33 +0800

    Update para2github.py

--- .github/workflows/para2github.py
+++ .github/workflows/para2github.py
@@ -128,7 +128,7 @@ def process_translation(file_id: int, path: Path) -> dict[str, str]:
         zh_cn_dict[key] = value
 
     # 特殊处理：ftbquest 文件
-    if "ftbquest" in path.name:
+    if "integrated_mc" in path.name:
         zh_cn_dict = {
             key: value.replace(" ", "\u00A0") if "image" not in value else value
             for key, value in zip(keys, values)
