838e09444c2a3c0d3e7154267eb5077b560efb7a
nageih <zhwlang2008@outlook.com>
2025-04-04 00:59:02 +0800

    1.5.1

--- CNPack/kubejs/client_scripts/tool_tips.js
+++ CNPack/kubejs/client_scripts/tool_tips.js
@@ -1,6 +1,6 @@
 ItemEvents.tooltip(event => {
   event.add([global.itemsToRemove], Text.red('此物品已被禁用')),
-  event.add('sophisticatedbackpacks:feeding_upgrade', Text.red('This was disabled due to an item voiding bug.')),
-  event.add('sophisticatedbackpacks:advanced_feeding_upgrade', Text.red('This was disabled due to an item voiding bug.')),
-  event.add('create_enchantment_industry:disenchanter', Text.red('Pondering this will crash your game.'))
+  event.add('sophisticatedbackpacks:feeding_upgrade', Text.red('此物品已因BUg而被禁用。')),
+  event.add('sophisticatedbackpacks:advanced_feeding_upgrade', Text.red('此物品已因BUg而被禁用。')),
+  event.add('create_enchantment_industry:disenchanter', Text.red('思索会使你的游戏崩溃。'))
 })
