81c9636c5ebd9f8f4984fd7c9e2161e84479725f
Jiahe Yan <zhwlang2008@outlook.com>
2025-11-11 16:44:02 +0800

    fix

--- CNPack/kubejs/client_scripts/tool_tips.js
+++ CNPack/kubejs/client_scripts/tool_tips.js
@@ -1,6 +1,6 @@
 ItemEvents.tooltip(event => {
   event.add([global.itemsToRemove], Text.red('此物品已被禁用')),
-  event.add('sophisticatedbackpacks:feeding_upgrade', Text.red('此物品已因BUg而被禁用。')),
-  event.add('sophisticatedbackpacks:advanced_feeding_upgrade', Text.red('此物品已因BUg而被禁用。')),
+  event.add('sophisticatedbackpacks:feeding_upgrade', Text.red('此物品已因Bug而被禁用。')),
+  event.add('sophisticatedbackpacks:advanced_feeding_upgrade', Text.red('此物品已因Bug而被禁用。')),
   event.add('create_enchantment_industry:disenchanter', Text.red('思索会使你的游戏崩溃。'))
 })
--- CNPack/kubejs/startup_scripts/brewing.js
+++
@@ -1,3 +0,0 @@
-MoreJSEvents.registerPotionBrewing(event => {
-    event.removeByPotion(null, "minecraft:popped_chorus_fruit", null);
-})
